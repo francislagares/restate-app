8f6500383e5dd32044dc41e99201edec2c98721b
Francis Lagares <francis.lagares@gmail.com>
2024-12-29 18:21:52 +0100

    feat: create header section in Home screen

--- src/app/(root)/(tabs)/index.tsx
+++ src/app/(root)/(tabs)/index.tsx
@@ -1,17 +1,34 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import icons from '@/constants/icons';
+import { useAuthContext } from '@/lib/appwrite/contexts/global-provider';
 
 export default function Index() {
+  const { user } = useAuthContext();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text className='my-10 font-rubik text-3xl font-bold'>
-        Welcome to ReState
-      </Text>
-    </View>
+    <SafeAreaView className='h-full bg-white'>
+      <View className='px-5'>
+        <View className='mt-5 flex flex-row items-center justify-between'>
+          <View className='flex flex-row'>
+            <Image
+              source={{ uri: user?.avatar }}
+              className='size-12 rounded-full'
+            />
+
+            <View className='ml-2 flex flex-col items-start justify-center'>
+              <Text className='font-rubik text-xs text-black-100'>
+                Good Morning
+              </Text>
+              <Text className='font-rubik-medium text-base text-black-300'>
+                {user?.name}
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className='size-6' />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
